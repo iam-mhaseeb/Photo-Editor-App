fa2525dcae33c911d4c0a97a882bad02c3990d5c
Haseeb <haseeb.emailbox@gmail.com>
2018-01-20 16:28:39 +0500

    Image Editor Standalone Added

--- app/src/main/java/com/ideotechs/photoeditorapp/MainActivity.java
+++ app/src/main/java/com/ideotechs/photoeditorapp/MainActivity.java
@@ -1,12 +1,16 @@
 package com.ideotechs.photoeditorapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
+
+import net.alhazmy13.mediapicker.Image.ImagePicker;
+
+import java.util.List;
 
 import ly.img.android.ui.activities.CameraPreviewActivity;
 import ly.img.android.ui.activities.CameraPreviewIntent;
@@ -15,7 +19,7 @@ import ly.img.android.ui.activities.PhotoEditorIntent;
 public class MainActivity extends AppCompatActivity {
 
     public static int CAMERA_PREVIEW_RESULT = 1;
-    private Button cameraBtn,liveCameraBtn,editorBtn;
+    private ImageButton cameraBtn,liveCameraBtn,editorBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,9 +27,9 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        cameraBtn = (Button)findViewById(R.id.camera);
-        liveCameraBtn = (Button)findViewById(R.id.livecamera);
-        editorBtn = (Button)findViewById(R.id.editor);
+        cameraBtn = (ImageButton)findViewById(R.id.camera);
+        //liveCameraBtn = (ImageButton)findViewById(R.id.livecamera);
+        editorBtn = (ImageButton)findViewById(R.id.editor);
         cameraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,6 +45,20 @@ public class MainActivity extends AppCompatActivity {
                         .startActivityForResult(CAMERA_PREVIEW_RESULT);
             }
         });
+        editorBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new ImagePicker.Builder(MainActivity.this)
+                        .mode(ImagePicker.Mode.GALLERY)
+                        .compressLevel(ImagePicker.ComperesLevel.MEDIUM)
+                        .directory(ImagePicker.Directory.DEFAULT)
+                        .extension(ImagePicker.Extension.PNG)
+                        .scale(600, 600)
+                        .allowMultipleImages(false)
+                        .enableDebuggingMode(true)
+                        .build();
+            }
+        });
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, android.content.Intent data) {
@@ -50,6 +68,15 @@ public class MainActivity extends AppCompatActivity {
 
             Toast.makeText(this, "Image Save on: " + path, Toast.LENGTH_LONG).show();
 
+        }
+        if (requestCode == ImagePicker.IMAGE_PICKER_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> mPaths = (List<String>) data.getSerializableExtra(ImagePicker.EXTRA_IMAGE_PATH);
+            new PhotoEditorIntent(MainActivity.this)
+                .setSourceImagePath(mPaths.get(0))
+                .setExportDir(PhotoEditorIntent.Directory.DCIM, "PhotoEditorApp")
+                .setExportPrefix("PhotoEditorApp_result_")
+                .destroySourceAfterSave(true)
+                .startActivityForResult(CAMERA_PREVIEW_RESULT);
         }
     }
 
